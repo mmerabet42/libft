acedd044db1181a42fab0fd5b84c7420a775dff1
Mohammed MERABET <dgny50@gmail.com>
2017-12-31 02:55:24 +0100

    ft_event

--- includes/ft_event.h
+++ includes/ft_event.h
@@ -1,6 +1,11 @@
 
 #include "ft_list.h"
 
+/*
+** Simple event system, when the event is triggered all callbacks functions are
+** called in the same order ther were added.
+*/
+
 typedef struct	s_event
 {
 	t_list		*callbacks;
@@ -17,3 +22,31 @@ int				ft_event_remove(t_event *event, t_callback callback);
 
 void			ft_event_trigger(t_event *event, void *data);
 void			ft_event_release(t_event *event);
+
+/*
+** A little bit different, callbacks are called depending on their priority
+** channels.
+*/
+
+typedef struct	s_pevent
+{
+	t_btree		*callbacks;
+	int			trigger;
+}				t_pevent;
+
+typedef void (*t_pcallback)(t_pevent *event, void *data);
+
+t_pevent		*ft_pevent_new(void);
+void			ft_pevent_del(t_pevent **event);
+
+int				ft_pevent_add(t_prevent *event, t_pcallback callback);
+int				ft_pevent_addp(t_pevent *event,
+						int priority,
+						t_pcallback callback);
+int				ft_pevent_remove(t_pevent *event, t_pcallback callback);
+int				ft_pevent_removep(t_pevent *event,
+						int priority,
+						t_pcallback callback);
+
+void			ft_pevent_trigger(t_pevent *event, void *data);
+void			ft_pevent_release(t_pevent *event);
