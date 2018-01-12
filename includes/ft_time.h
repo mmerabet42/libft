f83948f59456ad5f9d9f2b2ff75f9c695ef5c7dd
Mohammed MERABET <mmerabet@e3r13p13.42.fr>
2018-01-12 18:53:34 +0100

    ft_time

--- includes/ft_time.h
+++ includes/ft_time.h
@@ -6,7 +6,7 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 18:50:05 by mmerabet          #+#    #+#             */
-/*   Updated: 2018/01/12 18:51:50 by mmerabet         ###   ########.fr       */
+/*   Updated: 2018/01/12 18:53:25 by mmerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 # define FT_TIME_H
 
 # include <time.h>
+
+typedef struct		s_timef
+{
+	unsigned int	hour;
+	unsigned int	min;
+	unsigned int	sec;
+	unsigned int	day;
+	unsigned int	year;
+	char			*cday;
+	char			*cmonth;
+}					t_timef;
 
 t_timef	*ft_timefnew(const time_t *t);
 void	ft_timefdel(t_timef **tf);
