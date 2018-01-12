acdc723ff3bdca1005a540685bedca41b4961d48
Mohammed MERABET <mmerabet@e3r12p20.42.fr>
2018-01-12 23:15:07 +0100

    ft_time

--- includes/ft_time.h
+++ includes/ft_time.h
@@ -6,7 +6,7 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 18:50:05 by mmerabet          #+#    #+#             */
-/*   Updated: 2018/01/12 18:53:25 by mmerabet         ###   ########.fr       */
+/*   Updated: 2018/01/12 21:53:04 by mmerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@ typedef struct		s_timef
 	unsigned int	min;
 	unsigned int	sec;
 	unsigned int	day;
+	unsigned int	month;
 	unsigned int	year;
 	char			*cday;
 	char			*cmonth;
 }					t_timef;
 
-t_timef	*ft_timefnew(const time_t *t);
-void	ft_timefdel(t_timef **tf);
+t_timef				*ft_timefnew(const time_t *t);
+void				ft_timefdel(t_timef **tf);
 
 #endif
--- srcs/ft_timefnew.c
+++ srcs/ft_timefnew.c
@@ -6,19 +6,37 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 18:47:44 by mmerabet          #+#    #+#             */
-/*   Updated: 2018/01/12 18:49:54 by mmerabet         ###   ########.fr       */
+/*   Updated: 2018/01/12 20:53:28 by mmerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_time.h"
 #include "ft_types.h"
+#include "ft_str.h"
 #include "ft_mem.h"
 
-t_timef	*ft_timefnew(const time_t *t)
+static unsigned int	timef_getmonth(const char *mth)
+{
+	static char	*mths[] = {
+		"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug",
+		"Sept", "Oct", "Nov", "Dec"
+	};
+	int			i;
+
+	if (!mth)
+		return (0);
+	i = -1;
+	while (++i < 12)
+		if (ft_strequ(mth, mths[i]))
+			return (i + 1);
+	return (0);
+}
+
+t_timef				*ft_timefnew(const time_t *t)
 {
 	t_timef	*tf;
 	char	*out;
-	
+
 	if (!(tf = ft_memalloc(sizeof(t_timef))))
 		return (NULL);
 	if (!(out = ctime(t)))
@@ -31,12 +49,13 @@ t_timef	*ft_timefnew(const time_t *t)
 	tf->sec = ft_atoui(out + 17);
 	tf->day = ft_atoui(out + 8);
 	tf->year = ft_atoui(out + 20);
-	tf->cday = ft_memdup((void *)out, sizeof(char) * 3);
-	tf->cmonth = ft_memdup((void *)(out + 4), sizeof(char) * 3);
+	tf->cday = ft_strndup(out, 3);
+	tf->cmonth = ft_strndup(out + 4, 3);
+	tf->month = timef_getmonth(tf->cmonth);
 	return (tf);
 }
 
-void	ft_timefdel(t_timef **tf)
+void				ft_timefdel(t_timef **tf)
 {
 	if (!tf || !*tf)
 		return ;
