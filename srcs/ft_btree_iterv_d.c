436f4e6973a0a78cbc1880640ec867b4fa0dc384
Mohammed MERABET <mmerabet@e3r12p18.42.fr>
2018-01-20 19:25:42 +0100

    ft_btree

--- srcs/ft_btree_iterv_d.c
+++ srcs/ft_btree_iterv_d.c
@@ -6,13 +6,13 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/19 22:30:31 by mmerabet          #+#    #+#             */
-/*   Updated: 2018/01/19 22:34:20 by mmerabet         ###   ########.fr       */
+/*   Updated: 2018/01/20 17:03:27 by mmerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_btree.h"
 
-static void	inner_btree_lov(t_btree *bt, t_btiter f, int nm[2], void *data)
+static void	inner_btree_lov(t_btree *bt, t_btiter_d f, int nm[2], void *data)
 {
 	if (!bt)
 		return ;
@@ -23,7 +23,7 @@ static void	inner_btree_lov(t_btree *bt, t_btiter f, int nm[2], void *data)
 	inner_btree_lov(bt->left, f, nm, data);
 }
 
-static void	ft_btree_levelorderv(t_btree *bt, t_btiter f, void *data)
+static void	ft_btree_levelorderv(t_btree *bt, t_btiter_d f, void *data)
 {
 	int	n;
 	int	i;
@@ -39,7 +39,7 @@ static void	ft_btree_levelorderv(t_btree *bt, t_btiter f, void *data)
 	}
 }
 
-void		ft_btree_itermv_d(t_btree *bt, t_btiter f, t_btmode mode,
+void		ft_btree_itermv_d(t_btree *bt, t_btiter_d f, t_btmode mode,
 						void *data)
 {
 	if (!bt || !f)
@@ -56,7 +56,7 @@ void		ft_btree_itermv_d(t_btree *bt, t_btiter f, t_btmode mode,
 		f(bt, data);
 }
 
-void		ft_btree_iterv_d(t_btree *bt, t_btiter f, void *data)
+void		ft_btree_iterv_d(t_btree *bt, t_btiter_d f, void *data)
 {
 	ft_btree_itermv_d(bt, f, BT_INORDER, data);
 }
