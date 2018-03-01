ee7c511d980bd003b58234f4f6d86b6d0d6f214b
Mohammed MERABET <mmerabet@e1r2p16.42.fr>
2018-03-01 20:55:03 +0100

    strbetween

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 19:43:06 by mmerabet          #+#    #+#             */
-/*   Updated: 2017/12/19 20:49:02 by mmerabet         ###   ########.fr       */
+/*   Updated: 2018/03/01 19:20:09 by mmerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strcmp(const char *a, const char *b)
 	const unsigned char	*ua;
 	const unsigned char	*ub;
 
+	if (!a || !b)
+		return (-1);
 	ua = (const unsigned char *)a;
 	ub = (const unsigned char *)b;
 	while (*ua && *ub && (*ua == *ub))
@@ -32,6 +34,8 @@ int	ft_strncmp(const char *a, const char *b, size_t n)
 	const unsigned char	*ua;
 	const unsigned char *ub;
 
+	if (!a || !b)
+		return (-1);
 	ua = (const unsigned char *)a;
 	ub = (const unsigned char *)b;
 	while (n-- && (*ua || *ub))
