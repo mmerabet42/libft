07485cbb2ab6a00e79bc9cce16b01428a9cf6bdf
Mohammed MERABET <dgny50@gmail.com>
2018-11-02 07:10:34 +0100

    Implementing capturing groups for ft_regex

--- main.c
+++ main.c
@@ -16,16 +16,18 @@ int main(int argc, char **argv)
 	ft_regex(RGX_ADD, "BRACKET1", "?[{[*[?[@BRACKET]|?![{(){}[]}]@or?]]@G}]", NULL);
 	ft_regex(RGX_ADD, "BRACKET2", "?[\\{*[?[@BRACKET]|?![{(){}[]}]@or?]}@G]", NULL);
 
-	n = ft_regex(RGX_GROUP | RGX_ID, argv[1], argv[2], &id, &groups, &matches);
+	n = ft_regex(RGX_GLOBAL, argv[1], argv[2], &matches);
 
-	t_list	*it = groups;
+/*	t_list	*it = groups;
 	while (it)
 	{
 		t_regex_group *grp = (t_regex_group *)it->content;
 		ft_printf("group: '%.*s' %d %d %d\n", grp->len, grp->str, grp->pos, grp->len, grp->id);
 		it = it->next;
-	}
-	ft_printf("%d %d match\n", n, id);
+	}*/
+	ft_printf("Regular expression: '%s'\nString: '%s'\n", argv[1], argv[2]);
+	ft_print_matches(argv[2], matches);
+	ft_printf("%d match\n", n, id);
 	
 	ft_regex(RGX_FREEGRP, NULL, NULL, &groups);
 	ft_regex(RGX_FREE, NULL, NULL, &matches);
