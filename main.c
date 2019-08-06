f3bc374bce8b111808b7346a9ed1933b5c3c0d9d
Mohammed Merabet <33690623+mmerabet42@users.noreply.github.com>
2019-08-06 17:53:52 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int func_ex(void *arg, t_lq_eng *eng)
 	return 0;
 }
 
-int main()
+int main(int argc, char **argv)
 {
 /*	t_lq_node *bracket =
 		lq_node("g", NULL, lq_quant(1, 1), NULL, NULL);
@@ -21,7 +21,8 @@ int main()
 					bracket,
 				NULL),
 			lq_quant(0, -1), NULL,
-		lq_node("s", ")", lq_quant(1, 1), NULL, NULL)));
+		lq_node("s", ")", lq_quant(1, 1), NULL,
+		lq_node("name", "ROUND_BRACKET", lq_quant(1, 1), NULL, NULL))));
 	
 	t_lq_node *curly_bracket =
 		lq_node("s", "{", lq_quant(1, 1), NULL,
@@ -30,7 +31,8 @@ int main()
 					bracket,
 				NULL),
 			lq_quant(0, -1), NULL,
-		lq_node("s", "}", lq_quant(1, 1), NULL, NULL)));
+		lq_node("s", "}", lq_quant(1, 1), NULL,
+		lq_node("name", "CURLY_BRACKET", lq_quant(1, 1), NULL, NULL))));
 
 	t_lq_node *square_bracket =
 		lq_node("s", "[", lq_quant(1, 1), NULL,
@@ -39,7 +41,8 @@ int main()
 					bracket,
 				NULL),
 			lq_quant(0, -1), NULL,
-		lq_node("s", "]", lq_quant(1, 1), NULL, NULL)));
+		lq_node("s", "]", lq_quant(1, 1), NULL,
+		lq_node("name", "SQUARE_BRACKET", lq_quant(1, 1), NULL, NULL))));
 
 	t_lq_node *pin_bracket =
 		lq_node("s", "<", lq_quant(1, 1), NULL,
@@ -48,7 +51,8 @@ int main()
 					bracket,
 				NULL),
 			lq_quant(0, -1), NULL,
-		lq_node("s", ">", lq_quant(1, 1), NULL, NULL)));
+		lq_node("s", ">", lq_quant(1, 1), NULL,
+		lq_node("name", "PIN_BRACKET", lq_quant(1, 1), NULL, NULL))));
 
 	bracket->arg = round_bracket;
 	bracket->next_or =
@@ -91,7 +95,7 @@ int main()
 			lq_quant(1, 1), NULL,
 		lq_node("s", "hello", lq_quant(1, 1), NULL, NULL));
 	
-	t_lq_node *begin = delimsb;
+	t_lq_node *begin = bracket;
 		lq_node("s", "(", lq_quant(1, 1), NULL,
 		lq_node("g",
 				lq_node("g",
@@ -108,21 +112,146 @@ int main()
 		lq_node("s", ")", lq_quant(1, 1), NULL, NULL)));
 */
 
-	t_lq_node *rule_name =
-		lq_node("?", "a-zA-Z0-9_-", lq_quant(1, 1), NULL, NULL);
-
-	t_lq_node *quantifier =
+	t_lq_node *quantifier_a =
 		lq_node("s", "{", lq_quant(1, 1), NULL,
 		lq_node("?", " \t", lq_quant(0, -1), NULL,
-		lq_node("g"
-				lq_node("?", "0-9", lq_quant(0, -1), NULL, NULL),
+		lq_node("g",
+				lq_node("?", "0-9", lq_quant(0, -1), NULL,
+				lq_node("name", "MIN_MAX", lq_quant(1, 1), NULL, NULL)),
+			lq_quant(1, 1), NULL,
+		lq_node("s", " \t", lq_quant(0, -1), NULL,
+		lq_node("s", "}", lq_quant(1, 1), NULL,
+		lq_node("name", "QUANTIFIER_A", lq_quant(1, 1), NULL, NULL))))));
+
+	t_lq_node *quantifier_ab =
+		lq_node("s", "{", lq_quant(1, 1), NULL,
+		lq_node("?", " \t", lq_quant(0, -1), NULL,
+		lq_node("g",
+				lq_node("?", "0-9", lq_quant(0, -1), NULL,
+				lq_node("name", "MIN", lq_quant(1, 1), NULL, NULL)),
+			lq_quant(1, 1), NULL,
+		lq_node("?", " \t", lq_quant(0, -1), NULL,
+		lq_node("s", ",", lq_quant(1, 1), NULL,
+		lq_node("s", " \t", lq_quant(0, -1), NULL,
+		lq_node("g",
+				lq_node("?", "0-9", lq_quant(0, -1), NULL,
+				lq_node("name", "MAX", lq_quant(1, 1), NULL, NULL)),
+			lq_quant(1, 1), NULL,
+		lq_node("s", " \t", lq_quant(0, -1), NULL,
+		lq_node("s", "}", lq_quant(1, 1), NULL,
+		lq_node("name", "QUANTIFIER_AB", lq_quant(1, 1), NULL, NULL))))))))));
+
+	t_lq_node *quantifier_c =
+		lq_node("s", "*", lq_quant(1, 1),
+			lq_node("s", "+", lq_quant(1, 1),
+				lq_node("s", "?", lq_quant(1, 1), NULL,
+				lq_node("name", "MARK", lq_quant(1, 1), NULL, NULL)),
+			lq_node("name", "PLUS", lq_quant(1, 1), NULL, NULL)),
+		lq_node("name", "WILDCARD", lq_quant(1, 1), NULL, NULL));
+
+	t_lq_node *quantifier =
+		lq_node("r", quantifier_c, lq_quant(1, 1),
+			lq_node("r", quantifier_a, lq_quant(1, 1),
+				lq_node("r", quantifier_ab, lq_quant(1, 1), NULL, NULL),
+			NULL),
+		NULL); 
+
+	t_lq_node *node_rule =
+		lq_node("?", "a-zA-Z0-9_-", lq_quant(1, -1), NULL,
+		lq_node("name", "RULE_NAME", lq_quant(1, 1), NULL, NULL));
+
+	t_lq_node *backslash_char =
+		lq_node("s", "\\", lq_quant(1, 1), NULL,
+		lq_node("?", "", lq_quant(1, 1), NULL,
+		lq_node("name", "BACKSLASH_CHAR", lq_quant(1, 1), NULL, NULL)));
+
+	t_lq_node *quote =
+		lq_node("s", "'", lq_quant(1, 1), NULL,
+		lq_node("r",
+				lq_node("g", backslash_char, lq_quant(1, 1),
+					lq_node("g",
+							lq_node("!?", "'", lq_quant(1, 1), NULL,
+							lq_node("name", "CHAR", lq_quant(1, 1), NULL, NULL)),
+						lq_quant(1, 1), NULL, NULL),
+				NULL),
+			lq_quant(0, -1), NULL,
+		lq_node("s", "'", lq_quant(1, 1), NULL,
+		lq_node("name", "QUOTE", lq_quant(1, 1), NULL, NULL))));
+
+	t_lq_node *inner_bracket =
+		lq_node("r",
+				lq_node("g", backslash_char, lq_quant(1, 1),
+					lq_node("g",
+							lq_node("!?", "]", lq_quant(1, 1), NULL,
+							lq_node("name", "CHAR", lq_quant(1, 1), NULL, NULL)),
+						lq_quant(1, 1), NULL, NULL),
+				NULL), 
+			lq_quant(0, -1), NULL,
+		lq_node("name", "ARGUMENT", lq_quant(1, 1), NULL, NULL));
+
+	t_lq_node *node_bracket =
+		lq_node("s", "[", lq_quant(1, 1), NULL,
+		lq_node("r",
+				lq_node("g", node_rule, lq_quant(1, 1),
+					lq_node("g",
+							lq_node("name", "DEFAULT", lq_quant(1, 1), NULL, NULL),
+						lq_quant(1, 1), NULL,
+					lq_node("s", "@", lq_quant(0, 1), NULL, NULL)),
+				lq_node("s", "@", lq_quant(1, 1), NULL, NULL)),
 			lq_quant(1, 1), NULL,
 		lq_node("r",
-				lq_node("s", ",", lq_quant(0, 1), NULL,
-				lq_node("g")),
-			lq_quant(0, 1), NULL, NULL))));
-	t_lq_node *begin;
+				lq_node("g", quote, lq_quant(1, 1),
+					lq_node("g", inner_bracket, lq_quant(1, 1), NULL, NULL),
+				NULL),
+			lq_quant(1, 1), NULL,
+		lq_node("s", "]", lq_quant(1, 1), NULL,
+		lq_node("name", "BRACKET", lq_quant(1, 1), NULL, NULL)))));
 
+	t_lq_node *node_dot =
+		lq_node("s", ".", lq_quant(1, 1), NULL,
+		lq_node("name", "DOT", lq_quant(1, 1), NULL, NULL));
+
+	t_lq_node *node_boundary =
+		lq_node("?", "^$", lq_quant(1, 1), NULL,
+		lq_node("?", "nw", lq_quant(0, 1), NULL,
+		lq_node("name", "BOUNDARY", lq_quant(1, 1), NULL, NULL)));
+
+	t_lq_node *node =
+		lq_node("g",
+				lq_node("r", node_rule, lq_quant(1, 1),
+					lq_node("r", node_bracket, lq_quant(1, 1),
+						lq_node("r", node_dot, lq_quant(1, 1),
+							lq_node("r", quote, lq_quant(1, 1),
+								lq_node("r", node_boundary, lq_quant(1, 1), NULL, NULL),
+							NULL),
+						NULL),
+					NULL),
+				NULL),
+			lq_quant(1, 1), NULL,
+		lq_node("?", " \t", lq_quant(0, -1), NULL,
+		lq_node("g", quantifier, lq_quant(0, 1), NULL,
+		lq_node("name", "NODE", lq_quant(1, 1), NULL, NULL))));
+
+	t_lq_node *expression =
+		lq_node("r",
+				lq_node("?", " \t", lq_quant(0, -1), NULL,
+				lq_node("g", node, lq_quant(1, 1), NULL,
+				lq_node("?", " \t", lq_quant(0, -1), NULL,
+				lq_node("g",
+						lq_node("s", "|", lq_quant(1, 1), NULL,
+						lq_node("name", "OR", lq_quant(1, 1), NULL, NULL)),
+					lq_quant(0, 1), NULL,
+				lq_node("?", " \t", lq_quant(0, -1), NULL, NULL))))),
+			lq_quant(0, -1), NULL,
+		lq_node("name", "EXPRESSION", lq_quant(1, 1), NULL, NULL));
+
+	t_lq_node *begin =// lq_node("g", expression, lq_quant(1, 1), NULL, NULL);
+		lq_node("g",
+				lq_node("?", "a-z", lq_quant(0, -1), NULL, NULL),
+			lq_quant(1, 1), NULL,
+		lq_node("g",
+				lq_node("?", "a-z", lq_quant(0, -1), NULL, NULL),
+			lq_quant(1, 1), NULL, NULL));
 //	char *s = "bololfihvfhello";
 //	char *s = "(l({ook}o[l])l<(he)[l{}]lo>)";
 //	char *s = "   ([h]d{<a>d}((((((((((((((((((((e)))))))))))<b>)<c>))))))))dd) ookf erb";
@@ -131,7 +260,7 @@ int main()
 //	char *s = "hhhhhello";
 //	char *s = "Hello world";
 //	char *s = "Hello      ";
-	char *s = "9*987+-1";
+	char *s = argv[1];
 	int pos = 0;
 	int false_ret = 0;
 	t_lq_list *groups = NULL;
